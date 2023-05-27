4953274a786b8c98ce736accf0591580c39dd3a4
pvpadrao <pv.padrao@gmail.com>
2023-05-27 13:03:06 -0400

    testing

--- irobot_create_ignition/irobot_create_ignition_bringup/launch/create3_custom_cmd.launch.py
+++ irobot_create_ignition/irobot_create_ignition_bringup/launch/create3_custom_cmd.launch.py
@@ -1,12 +1,24 @@
 from launch import LaunchDescription
 from launch.substitutions import FindExecutable
 from launch.actions import ExecuteProcess
+from time import sleep
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    ld.add_action(
-    ExecuteProcess(
+
+    undock=ExecuteProcess(
+        cmd=[[
+            FindExecutable(name='ros2'),
+            " action send_goal ",
+            "/undock ",
+            "irobot_create_msgs/action/UnDock ",
+            '"{}"',
+        ]],
+        shell=True
+        )
+    
+    dock=ExecuteProcess(
         cmd=[[
             FindExecutable(name='ros2'),
             " action send_goal ",
@@ -15,7 +27,71 @@ def generate_launch_description():
             '"{}"',
         ]],
         shell=True
-    )
-)
+        )
+    
+    move_forward=ExecuteProcess(
+        cmd=[[
+            FindExecutable(name='ros2'),
+            " action send_goal ",
+            "/drive_distance ",
+            "irobot_create_msgs/action/DriveDistance ",
+            '"{distance: 0.5,max_translation_speed: 1.0}"',
+        ]],
+        shell=True
+        )
+    
 
+    move_fwd=ExecuteProcess(
+        cmd=[[
+            FindExecutable(name='ros2'),
+            " topic pub ",
+            "/cmd_vel ",
+            "geometry_msgs/msg/Twist ",
+            '"{linear: {x: -1.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"',
+        ]],
+        shell=True
+        )
+    
+    stop=ExecuteProcess(
+        cmd=[[
+            FindExecutable(name='ros2'),
+            " topic pub ",
+            "/cmd_vel ",
+            "geometry_msgs/msg/Twist ",
+            '"{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"',
+        ]],
+        shell=True
+        )
+    
+    
+
+    rotate_ccw=ExecuteProcess(
+        cmd=[[
+            FindExecutable(name='ros2'),
+            " action send_goal ",
+            "/rotate_angle ",
+            "irobot_create_msgs/action/RotateAngle ",
+            '"{angle: 1.57,max_rotation_speed: 0.5}"',
+        ]],
+        shell=True
+        )
+    
+    rotate_cw=ExecuteProcess(
+        cmd=[[
+            FindExecutable(name='ros2'),
+            " action send_goal ",
+            "/rotate_angle ",
+            "irobot_create_msgs/action/RotateAngle ",
+            '"{angle: -1.57,max_rotation_speed: 0.5}"',
+        ]],
+        shell=True
+        )
+
+
+    #ld.add_action(undock)
+    #sleep(5)
+    
+    ld.add_action(move_fwd)
+    #sleep(3)
+    #ld.add_action(stop)
     return ld
